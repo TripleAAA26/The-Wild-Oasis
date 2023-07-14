a279d68c70f553edc1eb0f06da54dddc1e1db7da
tripleAAA26 <96.azim.96@gmail.com>
2023-07-14 11:42:55 +0500

    styled-components Props and css functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import styled from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles.js"
 import Button from "./ui/Button.jsx"
 import Input from "./ui/Input.jsx"
+import Heading from "./ui/Heading.jsx"
 
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background-color: yellow;
-`
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -20,11 +16,12 @@ function App() {
         <>
             <GlobalStyles/>
             <StyledApp>
-                <H1>
-                    The Wild Oasis
-                </H1>
+                <Heading as='h1'>The Wild Oasis</Heading>
+                <Heading as='h2'>Check in and out</Heading>
                 <Button onClick={()=> alert('check in')}>Check in</Button>
                 <Button onClick={()=> alert('check out')}>Check out</Button>
+                <Heading as='h3' >Form</Heading>
+                <Input type='number' placeholder='Number of guests' />
                 <Input type='number' placeholder='Number of guests' />
             </StyledApp>
         </>
