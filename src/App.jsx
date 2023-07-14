315918708c2bd089edaaf035aae085982b3d833a
tripleAAA26 <96.azim.96@gmail.com>
2023-07-14 12:50:49 +0500

    building reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import GlobalStyles from "./styles/GlobalStyles.js"
 import Button from "./ui/Button.jsx"
 import Input from "./ui/Input.jsx"
 import Heading from "./ui/Heading.jsx"
+import Row from "./ui/Row.jsx"
 
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  
   padding: 20px;
   
 `
@@ -16,13 +17,33 @@ function App() {
         <>
             <GlobalStyles/>
             <StyledApp>
-                <Heading as='h1'>The Wild Oasis</Heading>
-                <Heading as='h2'>Check in and out</Heading>
-                <Button onClick={()=> alert('check in')}>Check in</Button>
-                <Button onClick={()=> alert('check out')}>Check out</Button>
-                <Heading as='h3' >Form</Heading>
-                <Input type='number' placeholder='Number of guests' />
-                <Input type='number' placeholder='Number of guests' />
+                <Row>
+                    <Row type='horizontal'>
+                        <Heading as='h1'>The Wild Oasis</Heading>
+                        <div>
+                            <Heading as='h2'>Check in and out</Heading>
+                            <Button
+                                onClick={()=> alert('check in')}
+                            >
+                                Check in
+                            </Button>
+                            <Button
+                                variation='secondary'
+                                size='small'
+                                onClick={()=> alert('check out')}
+                            >
+                                Check out
+                            </Button>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Heading as='h3' >Form</Heading>
+                        <form>
+                            <Input type='number' placeholder='Number of guests' />
+                            <Input type='number' placeholder='Number of guests' />
+                        </form>
+                    </Row>
+                </Row>
             </StyledApp>
         </>
     )
