fdf6d82c7df92b67b939ea36d348b4f9fd15c185
tripleAAA26 <96.azim.96@gmail.com>
2023-07-18 13:25:43 +0500

    Duplicating cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,8 @@ import {useState} from "react"
 import {formatCurrency} from "../../utils/helpers.js"
 import CreateCabinForm from "./CreateCabinForm.jsx"
 import {useDeleteCabin} from "./useDeleteCabin.js"
+import {HiPencil, HiSquare2Stack, HiTrash} from "react-icons/hi2"
+import {useCreateCabin} from "./useCreateCabin.js"
 
 const TableRow = styled.div`
   display: grid;
@@ -48,9 +50,27 @@ const Discount = styled.div`
 function CabinRow({cabin}) {
     const [showForm, setShowForm] = useState(false)
     const {isDeleting, deleteCabin} = useDeleteCabin()
+    const {isCreating, createCabin} = useCreateCabin()
 
-    const {id: cabinId, name, maxCapacity, regularPrice, discount, image} = cabin
+    const {id: cabinId,
+        name,
+        maxCapacity,
+        regularPrice,
+        discount,
+        image,
+        description,
+    } = cabin
 
+    function handleDuplicate() {
+        createCabin({
+            name: `Copy of ${name}`,
+            maxCapacity,
+            regularPrice,
+            discount,
+            image,
+            description,
+        })
+    }
 
     return (
         <>
@@ -65,15 +85,21 @@ function CabinRow({cabin}) {
                 }
                 <div>
                     <button
+                        onClick={handleDuplicate}
+                        disabled={isCreating}
+                    >
+                        <HiSquare2Stack />
+                    </button>
+                    <button
                         onClick={() => setShowForm(show=> !show)}
                     >
-                        Edit
+                        <HiPencil />
                     </button>
                     <button
                         onClick={() => deleteCabin(cabinId)}
                         disabled={isDeleting}
                     >
-                        Delete
+                        <HiTrash />
                     </button>
                 </div>
             </TableRow>
