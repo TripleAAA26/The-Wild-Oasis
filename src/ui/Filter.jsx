54f352a3830acca6f35156a12f7a3c56c71370d1
tripleAAA26 <96.azim.96@gmail.com>
2023-07-20 19:55:35 +0500

    Client-Side Filtering: Filtering Cabins

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,4 +1,5 @@
-import styled, { css } from "styled-components";
+import styled, {css} from "styled-components"
+import {useSearchParams} from "react-router-dom"
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -8,18 +9,18 @@ const StyledFilter = styled.div`
   padding: 0.4rem;
   display: flex;
   gap: 0.4rem;
-`;
+`
 
 const FilterButton = styled.button`
   background-color: var(--color-grey-0);
   border: none;
 
   ${(props) =>
-    props.active &&
-    css`
-      background-color: var(--color-brand-600);
-      color: var(--color-brand-50);
-    `}
+          props.active &&
+          css`
+            background-color: var(--color-brand-600);
+            color: var(--color-brand-50);
+          `}
 
   border-radius: var(--border-radius-sm);
   font-weight: 500;
@@ -32,4 +33,30 @@ const FilterButton = styled.button`
     background-color: var(--color-brand-600);
     color: var(--color-brand-50);
   }
-`;
+`
+
+
+function Filter({filterField, options}) {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const currentFilter = searchParams.get(filterField) || options.at(0).value
+    function handleClick(value) {
+        searchParams.set(filterField, value)
+        setSearchParams(searchParams)
+    }
+
+    return (
+        <StyledFilter>
+            {options.map( option =>
+                <FilterButton
+                    onClick={() => handleClick(option.value)}
+                    key={option.value}
+                    active={option.value === currentFilter}
+                >
+                    {option.label}
+                </FilterButton>
+            )}
+        </StyledFilter>
+    )
+}
+
+export default Filter
