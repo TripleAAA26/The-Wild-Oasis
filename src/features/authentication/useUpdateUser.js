24fe2aacd6f6b64544a1461f37137a3f00eb1bf1
tripleAAA26 <96.azim.96@gmail.com>
2023-07-25 11:52:56 +0500

    Implementing Dark Mode with CSS variables

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -9,10 +9,7 @@ export function useUpdateUser() {
         mutationFn: updateCurrentUser,
         onSuccess: ({user}) => {
             toast.success('User account successfully updated')
-
-            queryClient.invalidateQueries({
-                queryKey: ['user']
-            })
+            queryClient.setQueryData(['user'], user)
         },
         onError: err => toast.error(err.message)
     })
