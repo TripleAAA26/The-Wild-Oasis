be83cb17e58c106b7d83330e7f7a964e98e31054
tripleAAA26 <96.azim.96@gmail.com>
2023-07-25 19:33:23 +0500

    Displaying Statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import {useRecentBookings} from "./useRecentBookings.js"
 import Spinner from "../../ui/Spinner.jsx"
 import {useRecentStays} from "./useRecentStays.js"
+import Stats from "./Stats.jsx"
+import {useCabins} from "../cabins/useCabins.js"
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -13,13 +15,20 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
     const {bookings, isLoading: isLoading1} = useRecentBookings()
-    const {stays, isLoading: isLoading2, confirmedStays} = useRecentStays()
+    const {stays, isLoading: isLoading2, confirmedStays, numDays
+    } = useRecentStays()
+    const {cabins, isLoading: isLoading3} = useCabins()
 
     if(isLoading1 || isLoading2) return <Spinner />
 
     return (
         <StyledDashboardLayout>
-            <div>Statistics</div>
+            <Stats
+                bookings={bookings}
+                confirmedStays={confirmedStays}
+                numDays={numDays}
+                cabinCount={cabins.length}
+            />
             <div>Today activity</div>
             <div>Chart stay duration</div>
             <div>Chart Sales</div>
--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -18,8 +18,8 @@ export function useRecentStays() {
     })
 
     const confirmedStays = stays?.filter(stay =>
-        stay.status === 'check-in' || stay.status === 'check-out'
+         stay.status === 'checked-in' || stay.status === 'checked-out'
     )
 
-    return {isLoading, stays, confirmedStays}
+    return {isLoading, stays, confirmedStays, numDays}
 }
