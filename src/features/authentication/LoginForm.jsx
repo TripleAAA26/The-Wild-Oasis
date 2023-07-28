7e7bd1c30c183434e08060dd0a07f5c07db3fe0d
tripleAAA26 <96.azim.96@gmail.com>
2023-07-28 16:31:47 +0500

    Adding TOML and MD files

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import {useLogin} from "./useLogin.js"
 import SpinnerMini from "../../ui/SpinnerMini.jsx"
 
 function LoginForm() {
-    const [email, setEmail] = useState("azim@example.com")
-    const [password, setPassword] = useState("qweqweqwe")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const {login, isLoading} = useLogin()
 
     function handleSubmit(e) {
